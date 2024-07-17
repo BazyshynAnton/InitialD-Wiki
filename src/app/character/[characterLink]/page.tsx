30688a79e20b1913b78f35e3a8bee86dc4ece3af
Bazyshyn Anton <120124298+BazyshynAnton@users.noreply.github.com>
2024-07-17 11:00:53 +0300

    blank page issue

--- src/app/character/[characterLink]/page.tsx
+++ src/app/character/[characterLink]/page.tsx
@@ -4,11 +4,17 @@ import { promises as fs } from 'fs'
 import { Character } from '@/types/character/characterInfo/characterInfoTypes'
 
 export default async function AboutCharacter() {
-  const data = await fs.readFile(
-    process.cwd() + '/public/data/characters/characters.json',
-    'utf8'
-  )
-  const characters: Character[] = JSON.parse(data)
+  let characters: Character[]
+  try {
+    const data = await fs.readFile(
+      process.cwd() + '/public/data/characters/characters.json',
+      'utf8'
+    )
+    characters = JSON.parse(data)
+  } catch (error) {
+    console.error('Error reading or parsing JSON:', error)
+    return <div>Error loading character data</div>
+  }
 
   return <CharacterInfoPage characters={characters} />
 }
--- src/components/characterPage/characterInfoPage/CharacterInfoPage.tsx
+++ src/components/characterPage/characterInfoPage/CharacterInfoPage.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import ClientLoader from '@/components/shared/reusableComponents/clientLoader/ClientLoader'
+
 import { Character } from '@/types/character/characterInfo/characterInfoTypes'
 import { Image, usePathname, Link } from '@/components/shared/nextjsImports'
 
 import styles from '@/styles/characterPage/characterInfoPage/CharacterInfoPage.module.scss'
-import ClientLoader from '@/components/shared/reusableComponents/clientLoader/ClientLoader'
 
 export default function CharacterInfoPage({
   characters,
@@ -20,48 +21,53 @@ export default function CharacterInfoPage({
     currLink
   )
 
-  if (!character) {
-    return <ClientLoader />
-  }
-
   return (
-    <div className={styles.characterContainer}>
-      <span className={styles.characterBackground}></span>
-      <Link href="/character" className={styles.closeCharacterInfo} />
-      <div className={styles.characterVisualWrapper}>
-        <div className={styles.characterFigure}>
-          <Image
-            className={styles.characterImg}
-            width={1000}
-            height={1000}
-            src={character.characterImg}
-            alt="characterCar"
-            loading="eager"
-          />
+    <>
+      {!character ? (
+        <ClientLoader />
+      ) : (
+        <div className={styles.characterContainer}>
+          <span className={styles.characterBackground}></span>
+          <Link href="/character" className={styles.closeCharacterInfo} />
+          <div className={styles.characterVisualWrapper}>
+            <div className={styles.characterFigure}>
+              <Image
+                className={styles.characterImg}
+                width={1000}
+                height={1000}
+                src={character.characterImg}
+                alt="characterCar"
+                loading="eager"
+              />
+            </div>
+          </div>
+          <div className={styles.contentOfCharacter}>
+            <div className={styles.characterTextInfo}>
+              <h3>{character.name}</h3>
+              <p>{character.aboutCharacter}</p>
+              {character.carImg !== 'noCar' && (
+                <Image
+                  width={900}
+                  height={500}
+                  src={character.carImg}
+                  alt="characterCar"
+                  loading="eager"
+                />
+              )}
+              <h3>{character.carName}</h3>
+              <p>{character.aboutCar}</p>
+            </div>
+          </div>
+          <div className={styles.navigationArrows}>
+            <Link href={character.prevLink} className={styles.arrowLeft}></Link>
+            <Link
+              href={character.nextLink}
+              className={styles.arrowRight}
+            ></Link>
+          </div>
         </div>
-      </div>
-      <div className={styles.contentOfCharacter}>
-        <div className={styles.characterTextInfo}>
-          <h3>{character.name}</h3>
-          <p>{character.aboutCharacter}</p>
-          {character.carImg !== 'noCar' && (
-            <Image
-              width={900}
-              height={500}
-              src={character.carImg}
-              alt="characterCar"
-              loading="eager"
-            />
-          )}
-          <h3>{character.carName}</h3>
-          <p>{character.aboutCar}</p>
-        </div>
-      </div>
-      <div className={styles.navigationArrows}>
-        <Link href={character.prevLink} className={styles.arrowLeft}></Link>
-        <Link href={character.nextLink} className={styles.arrowRight}></Link>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
